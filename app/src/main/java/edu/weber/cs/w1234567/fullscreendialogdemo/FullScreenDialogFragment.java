b5e9a1f41b329c46d4042cbf8f7c4af2f9ab8722
Josh Jensen <joshuajensen1@weber.edu>
2020-10-10 00:28:28 -0600

    Added Fullscreen Dialog Demo

---
+++ app/src/main/java/edu/weber/cs/w1234567/fullscreendialogdemo/FullScreenDialogFragment.java
@@ -0,0 +1,75 @@
+package edu.weber.cs.w1234567.fullscreendialogdemo;
+
+import android.app.Dialog;
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.appcompat.widget.Toolbar;
+import androidx.fragment.app.DialogFragment;
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class FullScreenDialogFragment extends DialogFragment {
+
+    public FullScreenDialogFragment() {
+        // Required empty public constructor
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setStyle(DialogFragment.STYLE_NORMAL, R.style.AppTheme_Dialog_FullScreen);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_full_screen_dialog, container, false);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        requireDialog().getWindow().setWindowAnimations(R.style.AppTheme_DialogAnimation);
+
+        Toolbar toolbar = view.findViewById(R.id.toolbar);
+        toolbar.setNavigationOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                dismiss();
+            }
+        });
+
+        toolbar.setTitle(R.string.fullscreen_title);
+        toolbar.inflateMenu(R.menu.fullscreen_dialog);
+        toolbar.setOnMenuItemClickListener(new Toolbar.OnMenuItemClickListener() {
+            @Override
+            public boolean onMenuItemClick(MenuItem item) {
+                switch (item.getItemId()) {
+                    case R.id.save:
+                        //DO SOMETHING.
+                        return true;
+                    default:
+                        return false;
+                }
+            }
+        });
+    }
+
+    @NonNull
+    @Override
+    public Dialog onCreateDialog(@Nullable Bundle savedInstanceState) {
+        Dialog dialog = super.onCreateDialog(savedInstanceState);
+
+        dialog.getWindow().setWindowAnimations(R.style.AppTheme_DialogAnimation);
+
+        return dialog;
+    }
+}
